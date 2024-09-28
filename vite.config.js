ae61107b227ccdef9ca458ee83e52de8e1233248
Kevin Ogunlowo <kevinogunlowo273@gmail.com>
2024-09-28 10:01:04 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
-})
+
+  
+  root: 'C:/React class project/test_portfolio/client/my-react-app', // Your project root
+  base: '/',
+});
+
+  
+
