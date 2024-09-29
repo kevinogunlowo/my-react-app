c0398042f2cef52cbd69cf45ebd22471dd9a194e
Kevin Ogunlowo <kevinogunlowo273@gmail.com>
2024-09-29 10:54:56 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   plugins: [react()],
-
-  root: 'React class project',  // Sets the base directory for the Vite server
-  build: {
-    outDir: 'public',  // Output directory for built files
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
   },
 });
-
-  
-
